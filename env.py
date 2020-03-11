1f7b91a73abd7fa788d2e4c66dc97d682282fc90
Joe Sudron <joesudron@outlook.com>
2020-03-11 16:49:41 +0000

    created .gitignore & placed env.py inside

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["MONGO_DBNAME"] = 'lights-out'
-os.environ["MONGO_URI"] = 'mongodb+srv://joe-sudron:2PACjack83@myfirstcluster-eqlcx.mongodb.net/lights-out?retryWrites=true&w=majority'
-os.environ["SECRET_KEY"] = '2PACjack83'
