596c25ed99559cf3c887b12534a64ed51865c0c6
Joe Sudron <joesudron@outlook.com>
2020-03-11 16:31:49 +0000

    first test to connect to mongodb is successful

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'lights-out'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World...'
+@app.route('/get_stories')
+def get_stories():
+    return render_template("stories.html", stories=mongo.db.stories.find())
 
 
 if __name__ == '__main__':
