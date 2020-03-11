d2e1374d85cefd3c40878e1a33e3f25ebc022912
Joe Sudron <joesudron@outlook.com>
2020-03-11 17:35:41 +0000

    added hero image & text to home.html

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 
 @app.route('/get_stories')
