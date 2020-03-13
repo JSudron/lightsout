22133c979b93577be906c4461fc8bc0a4af53c25
Joe Sudron <joesudron@outlook.com>
2020-03-13 14:31:04 +0000

    rearranged the card grid system on stories.html & created a modal to display selected story, still working on functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,9 @@
 function adjustHeight(el){
     el.style.height = (el.scrollHeight > el.clientHeight) ? (el.scrollHeight)+"px" : "60px";
 }
+
+$.post("{{url_for('stories', stories_id=story._id)}}", function(data){
+
+    $("#myModal").html(data).fadeIn();
+
+});
